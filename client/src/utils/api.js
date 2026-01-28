2f3ac420ea55443893bc6f9aeca8d63f57514ebb
Steeve <57051366+steeve-496@users.noreply.github.com>
2026-01-28 11:48:19 +0530

    feat: Add Axios API client with JWT authentication and in-memory caching.

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://laymans-server.onrender.com/api', // Changed to production server
+    baseURL: 'http://ec2-43-205-228-13.ap-south-1.compute.amazonaws.com/api',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
